1cb9a11139c572a74bfb4841c0d2243e2e6bf447
Aktuganov Konstantin <aktuganovkonstantin@gmail.com>
2023-04-21 05:50:57 +0800

    Другой способ перезапустить apache

--- bin/install-apache.sh
+++ bin/install-apache.sh
@@ -13,11 +13,15 @@ if [ "$(docker ps -q -f name=project_server)" ]; then
   docker exec -it project_server bash -c "apt install libapache2-mod-php8.2 -y"
   docker exec -it project_server bash -c "apt install php8.2-fpm -y"
 
-  docker exec -it project_server bash -c "systemctl restart apache2.service"
+#  docker exec -it project_server bash -c "systemctl restart apache2.service"
+  docker exec -it project_server bash -c "service apache2 restart"
   echo "Скрипт успешно отработал и понаставил всякой херни в контейнеры"
+  sleep
+  pause
 else
     echo "Для работы скрипта нужно запустить Docker-контейнер project_server!"
     sleep
+    pause
 fi;
 sleep
 pause
