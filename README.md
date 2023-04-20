d0913945426043e87580ea0a2571cf80c8c37b70
Aktuganov Konstantin <aktuganovkonstantin@gmail.com>
2023-04-21 05:43:38 +0800

    Установка nginx при первом старте

--- README.md
+++ README.md
@@ -25,11 +25,8 @@
 1. скопировать содержимое /etc/hosts в конец своего hosts<br>
     * Windows: C:\Windows\System32\drivers\etc файл hosts<br>
     * Lixnu: /etc/hosts<br>
-
-[//]: # (2. ТОЛЬКО ЕСЛИ РАБОТА ПОД Windows:<br>)
-
-[//]: # (    * выполнить в консоли winget install GnuWin32.Make<br>)
 3. Выполнить команду docker-compose up и смотреть на магию<br>
-4. при первом запуске настроить bakend: ./bin/install-apache.sh
-4. по адресу http://phpmyadmin.loc:8080/ резко стал доступен phpmyadmin<br>
-5. по адресу http://server.loc/ резко стал доступен управляющий ресурс<br>
+4. при первом запуске настроить backend: ./bin/install-apache.sh<br>
+   Команда может просить иногда вводить в некоторые данные (временная зона, например)<br>
+5. по адресу http://phpmyadmin.loc:8080/ резко стал доступен phpmyadmin<br>
+6. по адресу http://server.loc/ резко стал доступен управляющий ресурс<br>
--- bin/install-apache.sh
+++ bin/install-apache.sh
@@ -4,9 +4,20 @@ if [ "$(docker ps -q -f name=project_server)" ]; then
   docker exec -it project_server bash -c "apt install ufw -y"
   docker exec -it project_server bash -c "ufw allow 80"
   docker exec -it project_server bash -c "ufw allow 443"
+
+  docker exec -it project_server bash -c "apt install nginx -y"
+  docker exec -it project_server bash -c "apt install software-properties-common -y"
+  docker exec -it project_server bash -c "add-apt-repository ppa:ondrej/php -y"
+  docker exec -it project_server bash -c "apt update -y"
+  docker exec -it project_server bash -c "apt install php8.2 -y"
+  docker exec -it project_server bash -c "apt install libapache2-mod-php8.2 -y"
+  docker exec -it project_server bash -c "apt install php8.2-fpm -y"
+
   docker exec -it project_server bash -c "systemctl restart apache2.service"
+  echo "Скрипт успешно отработал и понаставил всякой херни в контейнеры"
 else
     echo "Для работы скрипта нужно запустить Docker-контейнер project_server!"
     sleep
 fi;
 sleep
+pause
